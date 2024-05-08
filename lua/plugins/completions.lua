86d45a90da0bc8cd2ba9d77f5aa0130e19a6a581
Mizrahi, Avshalom <avshalom.mizrahi@intel.com>
2024-05-07 23:39:05 -0700

    fixing bug in snippet completion

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -18,7 +18,7 @@ return {
             cmp.setup({
                 snippet = {
                     expand = function(args)
-                        require('luasnip').lsp_expend(args.body)
+                        require('luasnip').lsp_expand(args.body)
                     end,
                 },
                 window = {
