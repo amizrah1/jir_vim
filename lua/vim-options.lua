1892b6d9fa8d1d6925bcd801365673db44cc5abd
Mizrahi, Avshalom <avshalom.mizrahi@intel.com>
2024-05-09 03:08:31 -0700

    adding option to copy paste from windows/linux clipboard by <leader>sy <leader>sp, also add gg going to first char in file

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -12,12 +12,19 @@ vim.cmd("set ignorecase")
 vim.cmd("set smartcase")
 vim.g.mapleader = " "
 
+-- due to nvim issue gg not going to first char in line
+vim.keymap.set('n', 'gg', "0gg")
+
 -- Pressing \\ shows the current directory
 vim.keymap.set('c', '\\\\', "<C-R>=expand('%:p:h').'/'<CR>")
 
 -- switch between buffers with ALT+up and ALT+down arrow
 vim.keymap.set('n', '<A-Up>',   ':bprev<CR>')
 vim.keymap.set('n', '<A-Down>', ':bnext<CR>')
+
+-- copy and paste from clipboard
+vim.keymap.set({'n', 'v'}, '<leader>sy', '"+y')
+vim.keymap.set('n', '<leader>sp', '"+p')
 
 -- change buffer size using CTRL+"+"
 vim.keymap.set('n', '+', '<C-W>+')
