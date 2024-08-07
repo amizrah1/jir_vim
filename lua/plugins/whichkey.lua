cfb62776fcd2f17ef361663e07a4fea33378065b
Mizrahi, Avshalom <avshalom.mizrahi@intel.com>
2024-08-07 05:11:57 -0700

    cleaning whichkey config

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -4,15 +4,6 @@ return {
         vim.o.timeout = true
         vim.o.timeoutlen = 300
         local whichkey = require("which-key")
-        whichkey.setup({
---            triggers_blacklist = {
---                -- list of mode / prefixes that should never be hooked by WhichKey
---                -- this is mostly relevant for keymaps that start with a native binding
---                c = { "\\" },
---                i = { "j", "k" },
---                v = { "j", "k" },
---            },
-        })
         whichkey.add({
             { "<leader>c", group = "Code operation" },
             { "<leader>f", group = "Telescope" },
