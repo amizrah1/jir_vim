f907bc279d5ca829091f3f08e4625fde58200d87
Mizrahi, Avshalom <avshalom.mizrahi@intel.com>
2024-08-18 23:19:30 -0700

    revert changes to lazy

--- lua/lazy_conf.lua
+++ lua/lazy_conf.lua
@@ -1,4 +1,4 @@
-local lazypath = "~amizrah1/.local/share/nvim/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
         "git",
