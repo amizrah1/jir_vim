aeab35af87de005a5424a24ffb7311260cee04d6
Mizrahi, Avshalom <avshalom.mizrahi@intel.com>
2024-08-25 05:15:38 -0700

    add mapping to align

--- lua/plugins/align.lua
+++ lua/plugins/align.lua
@@ -2,4 +2,8 @@ return {
     -- " In visual mode
     -- :'<,'>Align regex_pattern.*
 	{ "RRethy/nvim-align" },
+    vim.keymap.set('v', '<leader>a', function()
+        local char = vim.fn.input("Align by character: ")
+        vim.cmd("'<,'>Align " .. char)
+    end, { desc = 'align text in selected lines by char <input>' })
 }
